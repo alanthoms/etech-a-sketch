948711e03dbd8536681a15b427c7780c5cfc6614
Alan <alanthomas171@gmail.com>
2024-08-01 00:08:20 +0100

    Add div and square divs with simple css

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-
 document.addEventListener("DOMContentLoaded", () => {
     const container = document.querySelector(".container");
-
-    for (let i = 0; i < 4^2; i++) { // 16x16 = 256
+    let s = 4;
+    for (let i = 0; i < s * s; i++) { // 16x16 = 256
         const div = document.createElement("div");
         div.classList.add("square");
         container.appendChild(div);
