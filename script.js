039039a578edf5d814bb25f85d6eaf0af98517e1
Alan <alanthomas171@gmail.com>
2024-08-30 18:11:33 +0100

    Add inner loops and fixed flexbox

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
     const container = document.querySelector(".container");
     let s = 4;
-    for (let i = 0; i < s * s; i++) { // 16x16 = 256
-        const div = document.createElement("div");
-        div.classList.add("square");
-        container.appendChild(div);
+    for (let i = 0; i < s ;i++) { // 16x16 = 256
+        const rowDiv = document.createElement("div");
+        rowDiv.classList.add("row");
+        container.appendChild(rowDiv);
+        for (let j = 0; j < s; j++) { // 16x16 = 256
+            const sqDiv = document.createElement("div");
+            sqDiv.classList.add("square");
+            rowDiv.appendChild(sqDiv);
+        }
     }
 });
