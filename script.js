93687cc5eb68a4fd948b7da9bb2fb51344798ecb
Alan <alanthomas171@gmail.com>
2024-09-16 23:52:46 +0100

    Add javascript event listener for changing sqdiv bg color

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const container = document.querySelector(".container");
-    let s = 4;
+    let s = 16;
     for (let i = 0; i < s ;i++) { // 16x16 = 256
         const rowDiv = document.createElement("div");
         rowDiv.classList.add("row");
@@ -8,7 +8,14 @@ document.addEventListener("DOMContentLoaded", () => {
         for (let j = 0; j < s; j++) { // 16x16 = 256
             const sqDiv = document.createElement("div");
             sqDiv.classList.add("square");
+
+            //add event listener to squarediv of mouseenter so that the style background color of the div 
+            sqDiv.addEventListener("mouseenter", () => {
+                sqDiv.style.backgroundColor = "red"; // Change the color when hovered
+            });
             rowDiv.appendChild(sqDiv);
         }
     }
 });
+
+
